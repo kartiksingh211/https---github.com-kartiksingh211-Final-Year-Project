da4e5bec91cb1a31b8826e1030f8b27eaf3cee4f
kartiksingh211 <itspragativerma@gmail.com>
2026-04-09 12:39:18 +0530

    Delete project-management directory

--- project-management/server/routes/workspaceRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUserWorkspaces } from "../controllers/workspaceController.js";
-
-const workspaceRouter = express.Router();
-
-workspaceRouter.get("/", getUserWorkspaces);
-
-export default workspaceRouter;
